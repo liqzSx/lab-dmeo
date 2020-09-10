df752477c7de51a2eb9080b7d93526ceeadbcb17
LiQz <liqingzhe@enmonster.com>
2020-09-10 17:25:21 +0800

    my-lab-first-git

---
+++ src/main/java/com/lqz/labdmeo/LabDmeoApplication.java
@@ -0,0 +1,13 @@
+package com.lqz.labdmeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabDmeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabDmeoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lqz/labdmeo/LabDmeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lqz.labdmeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDmeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
